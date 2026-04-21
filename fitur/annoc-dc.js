c13ef2bc0a1192d34d1718c181b293ae78b76185
aliffajriadi <aliffajriadi@gmail.com>
2026-04-21 20:29:11 +0700

    huashd

--- fitur/annoc-dc.js
+++ fitur/annoc-dc.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import config from "../config";
+import config from "../config.js";
 
 const webhook = config.DISCORD_WEBHOOK_ANNOUC;
 export const sendPesan = async (pesan) => {
